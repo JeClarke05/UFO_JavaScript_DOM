19723f191624683b6c274ab8037946c246a153fc
Jen Clarke <39578135+JeClarke05@users.noreply.github.com>
2018-11-28 18:53:52 -0700

    updated app.py file

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
+// Get a reference to the table body
+var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+// Console.log the weather data from data.js
+console.log(data);
+data.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
