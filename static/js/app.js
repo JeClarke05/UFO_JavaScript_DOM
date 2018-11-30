305f731ef99a0a1a542b92e8fab58e02fe0761d4
Jen Clarke <39578135+JeClarke05@users.noreply.github.com>
2018-11-29 18:23:33 -0700

    added filter code to app.js

--- static/js/app.js
+++ static/js/app.js
@@ -12,3 +12,32 @@ data.forEach((ufoReport) => {
       cell.text(value);
     });
   });
+
+// Select the submit button
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(row => row.datetime === inputValue);
+
+  console.log(filteredData);
+
+  d3.select("#ufo-table")
+  .append("td").text(`Date: ${mean}`)
+  .append("li").text(`Median: ${median}`)
+  .append("li").text(`Mode: ${mode}`)
+  .append("li").text(`Variance: ${variance}`)
+  .append("li").text(`Standard Deviation: ${standardDeviation}`);
+});
